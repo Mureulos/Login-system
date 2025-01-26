e1e1e486bcf68b7fefd4a0f747527fe534351f4e
Mureulos <murilodesantana05@gmail.com>
2025-01-26 01:28:24 -0300

    Feat: Implementações inicias e criação das migrações para conexão com o DB

--- LoginSystem-backend/Controllers/AuthController.cs
+++ LoginSystem-backend/Controllers/AuthController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LoginSystem.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LoginSystem.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class AuthController : ControllerBase
     {
         [HttpGet]
---
+++ LoginSystem-backend/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LoginSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>(entity => { entity.HasIndex(e => e.Email).IsUnique();  });
+        }
+    }
+}
